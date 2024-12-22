45f72d1001aec7fdc05b5e048e520acace81ecfa
tin <phantrungtin28903@gmail.com>
2024-12-22 23:01:42 +0700

    ao crud menu

---
+++ Areas/Admin/Controllers/MenuController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ShopCake.Models;
+
+namespace ShopCake.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuController : Controller
+    {
+        private readonly CakeShopContext _context;
+
+        public MenuController(CakeShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Menu
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Menus.ToListAsync());
+        }
+
+        // GET: Admin/Menu/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus
+                .FirstOrDefaultAsync(m => m.MEN_ID == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // GET: Admin/Menu/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Menu/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MEN_ID,PARENT_ID,Title,Url,CreatedDate,createdBy,updatedDate,updatedBy")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(menu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menu);
+        }
+
+        // GET: Admin/Menu/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus.FindAsync(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: Admin/Menu/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MEN_ID,PARENT_ID,Title,Url,CreatedDate,createdBy,updatedDate,updatedBy")] Menu menu)
+        {
+            if (id != menu.MEN_ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(menu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MenuExists(menu.MEN_ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menu);
+        }
+
+        // GET: Admin/Menu/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus
+                .FirstOrDefaultAsync(m => m.MEN_ID == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // POST: Admin/Menu/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var menu = await _context.Menus.FindAsync(id);
+            if (menu != null)
+            {
+                _context.Menus.Remove(menu);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MenuExists(int id)
+        {
+            return _context.Menus.Any(e => e.MEN_ID == id);
+        }
+    }
+}
