17ee286b53583c1981b7da75e2cff50d16825470
tin <phantrungtin28903@gmail.com>
2025-01-03 20:13:40 +0700

    them gioi han DisplayOrder cho CategoryDTO

--- Areas/Admin/DTO/CategoryDTO.cs
+++ Areas/Admin/DTO/CategoryDTO.cs
@@ -1,10 +1,15 @@
-﻿using ShopCake.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using ShopCake.Models;
 
 namespace ShopCake.Areas.Admin.DTO
 {
     public class CategoryDTO :BaseModel
     {
         public string name { get; set;}
+
+        [Range(1, int.MaxValue, ErrorMessage = "Giá trị phải lớn hơn hoặc bằng 1.")]
+
         public int DisplayOrder { get; set; }
+
     }
 }
